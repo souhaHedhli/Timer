b2b9376981917ee07650f636a717ef804e2d4c72
souha <souha.hedhli@gmail.com>
2019-01-21 23:45:18 +0100

    change

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './App.css';
 import { Container, Row, Col } from 'reactstrap';
 
 let hour = '00'
-let menute = '30'
+let minute = '30'
 let second = '00'
 class App extends Component {
   render() {
     return (
       <Container className="App">
         <Row className='m-t-50'>
-          <Col className='font-xl'>{hour}:{menute}:{second}</Col>
+          <Col className='font-xl'>{hour}:{minute}:{second}</Col>
         </Row>
         <Row>
           <Col className='font-s gray float-right'>
           <span className='p-75'>Hour</span>
-          <span className='p-r-60'>Menute</span>
+          <span className='p-r-60'>Minute</span>
           <span className='p-r-50'>Second</span>
           </Col>
         </Row>
